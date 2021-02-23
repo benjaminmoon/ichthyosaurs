b9dc0c88c70a3b8578fe1a4ff6bbb60b92a0eeff
benjaminmoon <benjamin.moon@bristol.ac.uk>
2021-02-23 19:03:42 +0000

    very minor updates
    * add new record of _Undorosaurus kielanae_

--- ichthyosauromorph-taxonomy/write_synonymy.py
+++ ichthyosauromorph-taxonomy/write_synonymy.py
@@ -1,4 +1,9 @@
 #! /usr/bin/python
+'''Create taxonomy and synonymy lists from CSV files
+
+This script fills a LaTeX template file to produce a document showing a list of
+taxa and synonymy.
+'''
 
 import sys
 import csv
@@ -9,7 +14,7 @@ try:
     script_file = sys.argv[0]
     taxon_file, synonymy_file, outfile = sys.argv[1:]
 except ValueError:
-    print("Usage: {} taxon_file synonymy_file outfile".format(script_file))
+    print('Usage: {} taxon_file synonymy_file outfile'.format(script_file))
 
 # function to read in file given as script arguments
 # def read_file(filename):
@@ -20,8 +25,16 @@ except ValueError:
 #         print("Could not find input file: {}".format(filename))
 #         sys.exit()
 
-with open(taxon_file, newline = "") as taxa:
-    taxa = csv.reader(taxa, delimiter = "\t")
+with open(taxon_file, newline = '') as taxa, open(synonymy_file, newline = '') as synonymy:
+    taxa = csv.DictReader(taxa, delimiter = '\t')
+    synonymy = csv.DictReader(synonymy, delimiter = '\t')
     
-    for row in taxa:
-        print(row[0], "\cite{", row[1], "}")
+    for trow in taxa:
+        current_taxon = trow['accepted_name']
+
+        print(list(trow))
+        # synonymy[synonymy['accepted_name'] == current_taxon]
+            # print(srow)
+        # print(filtered)
+        # print(row['accepted_name'], '\cite{', row['accepted_authority'], '}')
+        # csv.writer(open(r'abx.csv', 'w'), delimiter = '\t').writerows(filtered)
