892c4369628d3f87288a37e00eada7cfb69dad91
Ben Moon <ben@bcmoon.uk>
2021-02-24 13:15:28 +0000

    working version of processing script
    * first version TeX file produced

--- ichthyosauromorph-taxonomy/write_synonymy.py
+++ ichthyosauromorph-taxonomy/write_synonymy.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-'''Create taxonomy and synonymy lists from CSV files
+'''Create taxonomy and synonymy lists from TSV files
 
 This script fills a LaTeX template file to produce a document showing a list of
 taxa and synonymy.
@@ -7,6 +7,7 @@ taxa and synonymy.
 
 import sys
 import csv
+import re
 
 # return error if the wrong number of arguments are given
 # usage is given in the error return value
@@ -16,25 +17,47 @@ try:
 except ValueError:
     print('Usage: {} taxon_file synonymy_file outfile'.format(script_file))
 
-# function to read in file given as script arguments
-# def read_file(filename):
-#     try:
-#         with open(filename) as f:
-#             return f.read()
-#     except FileNotFoundError:
-#         print("Could not find input file: {}".format(filename))
-#         sys.exit()
+taxon_name = '''\
+\\emph{accepted_name} \cite{accepted_authority}\n
+'''
 
-with open(taxon_file, newline = '') as taxa, open(synonymy_file, newline = '') as synonymy:
-    taxa = csv.DictReader(taxa, delimiter = '\t')
-    synonymy = csv.DictReader(synonymy, delimiter = '\t')
+synonym_row = '''\
+assignment_confidence & \\cite*{reference} & \\emph{identified_name} \\citeauthor*{identified_authority} \\\\
+'''
+
+def find_replace_multi(string, dictionary):
+    for item in dictionary.keys():
+        string = re.sub(item, dictionary[item], string)
+    return string
+
+def filter_taxon(row):
+    return current_taxon in row['accepted_name']
+
+# with open(taxon_file, newline = '') as taxa, open(synonymy_file, newline = '') as synonymy:
+#     taxa = csv.DictReader(taxa, delimiter = '\t')
+#     synonymy = csv.DictReader(synonymy, delimiter = '\t')
+
+with open(outfile, 'wt') as out_file:
     
-    for trow in taxa:
-        current_taxon = trow['accepted_name']
+    for taxon in csv.DictReader(open(taxon_file, newline = ''), delimiter = '\t'):
+        current_taxon = taxon['accepted_name']
+        this_taxon = find_replace_multi(taxon_name, taxon)
+        these_synonyms = str()
+    
+        for synonym in csv.DictReader(open(synonymy_file, newline = ''), delimiter = '\t'):
+            current_synonym = synonym['accepted_name']
+    
+            if current_synonym == current_taxon:
+                print('Synonym matched:', current_synonym, "=", current_taxon)
+    
+                these_synonyms = these_synonyms + find_replace_multi(synonym_row, synonym)
 
-        print(list(trow))
-        # synonymy[synonymy['accepted_name'] == current_taxon]
-            # print(srow)
-        # print(filtered)
-        # print(row['accepted_name'], '\cite{', row['accepted_authority'], '}')
-        # csv.writer(open(r'abx.csv', 'w'), delimiter = '\t').writerows(filtered)
+        if len(these_synonyms) > 0:
+            these_synonyms = '\\begin{tabularx}{42em}{llX}\n' + these_synonyms + '\\end{tabularx}\n\n'
+         
+        # print(this_taxon)
+        # print(these_synonyms)
+
+        out_file.write(this_taxon)
+        out_file.write(these_synonyms)
+
