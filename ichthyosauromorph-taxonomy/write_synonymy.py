5300554368764f515cc209b456cd866e78e4a5e3
benjaminmoon <benjamin.moon@bristol.ac.uk>
2021-02-24 17:00:43 +0000

    sort most processing
    * general table format achieved
    * TODO: sort by reference age
      1. subset synonymy for taxon
      2. parse reference bibtex keys to dates/years
      3. sort oldest to youngest

--- ichthyosauromorph-taxonomy/write_synonymy.py
+++ ichthyosauromorph-taxonomy/write_synonymy.py
@@ -18,11 +18,11 @@ except ValueError:
     print('Usage: {} taxon_file synonymy_file outfile'.format(script_file))
 
 taxon_name = '''\
-\\emph{accepted_name} \cite{accepted_authority}\n
+\\emph{accepted_name}~\cite{accepted_authority}\n
 '''
 
 synonym_row = '''\
-assignment_confidence & \\cite*{reference} & \\emph{identified_name} \\citeauthor*{identified_authority} \\\\
+assignment_confidence & \\cyear{reference} & \\emph{identified_name} \\cauth{identified_authority}; p~pageref locality_info \\\\
 '''
 
 def find_replace_multi(string, dictionary):
@@ -30,33 +30,59 @@ def find_replace_multi(string, dictionary):
         string = re.sub(item, dictionary[item], string)
     return string
 
-def filter_taxon(row):
-    return current_taxon in row['accepted_name']
-
-# with open(taxon_file, newline = '') as taxa, open(synonymy_file, newline = '') as synonymy:
-#     taxa = csv.DictReader(taxa, delimiter = '\t')
-#     synonymy = csv.DictReader(synonymy, delimiter = '\t')
+texy_strings = {
+        '* ' : '\textasterisk '
+}
 
 with open(outfile, 'wt') as out_file:
     
+    out_file.write('%! TEX root = test_tex.tex\n\n')
+
     for taxon in csv.DictReader(open(taxon_file, newline = ''), delimiter = '\t'):
         current_taxon = taxon['accepted_name']
         this_taxon = find_replace_multi(taxon_name, taxon)
         these_synonyms = str()
     
         for synonym in csv.DictReader(open(synonymy_file, newline = ''), delimiter = '\t'):
-            current_synonym = synonym['accepted_name']
+            current_synonym = synonym['identified_name']
     
-            if current_synonym == current_taxon:
-                print('Synonym matched:', current_synonym, "=", current_taxon)
+            if synonym['accepted_name'] == current_taxon:
+                print('Record matched:', current_synonym, "→", current_taxon)
     
-                these_synonyms = these_synonyms + find_replace_multi(synonym_row, synonym)
+                if synonym['identified_status'] == 'ncomb':
+                    synonym_row = re.sub('citeauthor\*', 'pciteauthor', synonym_row)
+
+                unit_separator = ', '
+
+                lithostrat_keys = ['bed', 'member', 'formation', 'zone']
+                lithostratigraphy = unit_separator.join([x for x in [synonym.get(key) for key in lithostrat_keys] if x])
+
+                chronostrat_keys = ['stage', 'series', 'system']
+                chronostratigraphy = unit_separator.join([x for x in [synonym.get(key) for key in chronostrat_keys] if x])
+
+                locality_info = str()
+                if len(chronostratigraphy) > 0 and len(lithostratigraphy) > 0:
+                    locality_info = lithostratigraphy + ' (' + chronostratigraphy + ')'
+                elif len(chronostratigraphy) > 0:
+                    locality_info = chronostratigraphy
+                elif len(lithostratigraphy) > 0:
+                    locality_info = lithostratigraphy
+
+                if len(synonym['location']) > 0 and len(locality_info) > 0:
+                    locality_info = locality_info + '; ' + synonym['location']
+                elif len(synonym['location']) > 0:
+                    locality_info = synonym['location']
+
+                if len(locality_info) > 0:
+                    locality_info = '[' + locality_info + '.] '
+                if len(synonym['comments']) > 0:
+                    locality_info = locality_info + synonym['comments']
+
+                this_synonym = find_replace_multi(synonym_row, synonym)
+                these_synonyms = these_synonyms + re.sub('locality_info', locality_info, this_synonym)
 
         if len(these_synonyms) > 0:
-            these_synonyms = '\\begin{tabularx}{42em}{llX}\n' + these_synonyms + '\\end{tabularx}\n\n'
-         
-        # print(this_taxon)
-        # print(these_synonyms)
+            these_synonyms = '\\begin{tabularx}{42em}{rlX}\n\\small\n' + these_synonyms + '\\end{tabularx}\n\n'
 
         out_file.write(this_taxon)
         out_file.write(these_synonyms)
